7cd40e74ddce1ef773d327d1074b3dc6041b70bd
Marcus Kruse <esurk.sucram@gmail.com>
2017-06-10 16:07:23 +0200

    Add Xema and stubs

--- lib/xema.ex
+++ lib/xema.ex
@@ -1,5 +1,32 @@
 defmodule Xema do
-  @moduledoc """
-  Xema.
+  @doc """
+  Xema ...
   """
+
+  defstruct type: :any, properties: nil
+
+  @types %{
+    any: Xema.Any,
+    null: Xema.Null,
+    boolean: Xema.Boolean,
+    object: Xema.Object,
+    array: Xema.Array,
+    number: Xema.Number,
+    string: Xema.String,
+    enum: Xema.Enum
+  }
+
+  @callback is_valid?(%Xema{}, any) :: boolean
+  @callback properties(any) :: struct
+
+  def create(), do: %Xema{}
+  def create(:string, properties \\ []) do
+    %Xema{type: :string, properties: Xema.String.properties(properties)}
+  end
+
+  for {type, xmodule} <- Map.to_list(@types) do
+    def is_valid?(%Xema{type: unquote(type)} = schema, value) do
+      unquote(xmodule).is_valid?(schema, value)
+    end
+  end
 end
