090d03bf82c949f00f139221d09607086c9bba4e
Marcus Kruse <esurk.sucram@gmail.com>
2017-06-10 10:19:26 +0200

    Add empty project

---
+++ lib/xema.ex
@@ -0,0 +1,18 @@
+defmodule Xema do
+  @moduledoc """
+  Documentation for Xema.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Xema.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xema_test.exs
@@ -0,0 +1,8 @@
+defmodule XemaTest do
+  use ExUnit.Case
+  doctest Xema
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
