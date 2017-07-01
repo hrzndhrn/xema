ccce6fd0e1d35c0266882cb528cf361d48ebd533
Marcus Kruse <esurk.sucram@gmail.com>
2017-07-01 10:18:45 +0200

    Rename Xema.Array to Xema.List

---
+++ lib/xema/list.ex
@@ -0,0 +1,92 @@
+defmodule Xema.List do
+  @moduledoc """
+  TODO
+  """
+
+  @behaviour Xema
+
+  defstruct items: nil,
+            min_items: nil,
+            max_items: nil,
+            additional_items: false,
+            unique_items: nil,
+            as: :list
+
+  alias Xema.List
+
+  @spec properties(list) :: %List{}
+  def properties(properties), do: struct(%Xema.List{}, properties)
+
+  @spec is_valid?(%List{}, any) :: boolean
+  def is_valid?(properties, list), do: validate(properties, list) == :ok
+
+  @spec validate(%List{}, any) :: :ok | {:error, any}
+  def validate(properties, list) do
+    with :ok <- type(properties, list),
+         :ok <- min_items(properties, list),
+         :ok <- max_items(properties, list),
+         :ok <- items(properties, list),
+         :ok <- unique(properties, list),
+      do: :ok
+  end
+
+  defp type(_properties, list) when is_list(list), do: :ok
+  defp type(properties, _list), do: {:error, %{type: properties.as}}
+
+  defp min_items(%List{min_items: nil}, _list), do: :ok
+  defp min_items(%List{min_items: min_items}, list)
+    when length(list) < min_items,
+    do: {:error, %{min_items: min_items}}
+  defp min_items(_properties, _list), do: :ok
+
+  defp max_items(%List{max_items: nil}, _list), do: :ok
+  defp max_items(%List{max_items: max_items}, list)
+    when length(list) > max_items,
+    do: {:error, %{max_items: max_items}}
+  defp max_items(_properties, _list), do: :ok
+
+  defp unique(%List{unique_items: nil}, _list), do: :ok
+  defp unique(%List{unique_items: true}, list) do
+    if is_unique?(list),
+      do: :ok,
+      else: {:error, :not_unique, %{}}
+  end
+
+  defp is_unique?(list, set \\ %{})
+  defp is_unique?([], _), do: true
+  defp is_unique?([h|t], set) do
+    case set do
+      %{^h => true} -> false
+      _ -> is_unique?(t, Map.put(set, h, true))
+    end
+  end
+
+  defp items(%List{items: nil}, _list), do: :ok
+  defp items(%List{items: items, additional_items: additional_items}, list)
+    when is_list(items),
+    do: items_tuple(items, additional_items, list, 0)
+  defp items(%List{items: items}, list) do
+    items_list(items, list, 0)
+  end
+
+  defp items_list(_schema, [], _at), do: :ok
+  defp items_list(schema, [item|list], at) do
+    case Xema.validate(schema, item) do
+      :ok -> items_list(schema, list, at + 1)
+      error -> {:error, :nested, %{at: at, error: error}}
+    end
+  end
+
+  defp items_tuple([], _additonal_items, [], _at), do: :ok
+  defp items_tuple(_schemas, _additonal_items, [], at),
+    do: {:error, :missing_value, %{at: at}}
+  defp items_tuple([], true, _additonal_items, at),
+    do: {:error, :extra_value, %{at: at}}
+  defp items_tuple([], false, _additonal_items, _at), do: :ok
+  defp items_tuple([schema|schemas], additional_items, [item|list], at) do
+    case Xema.validate(schema, item) do
+      :ok -> items_tuple(schemas, additional_items, list, at + 1)
+      error -> {:error, :nested, %{at: at, error: error}}
+    end
+  end
+end
---
+++ test/xema/list_test.exs
@@ -0,0 +1,101 @@
+defmodule Xema.ListTest do
+
+  use ExUnit.Case, async: true
+
+  import Xema, only: [is_valid?: 2, validate: 2]
+
+  alias Xema.List
+
+  setup do
+    %{
+      array: Xema.create(:list),
+      unique: Xema.create(:list, unique_items: true),
+      length: Xema.create(:list, min_items: 2, max_items: 3),
+      integers: Xema.create(:list, items: Xema.create(:integer)),
+      tuple: Xema.create(:list, items: [Xema.create(:string),
+                                        Xema.create(:integer)]),
+      tuple_add: Xema.create(:list,
+                             additional_items: true,
+                             items: [Xema.create(:string),
+                                     Xema.create(:integer)])
+    }
+  end
+
+  test "type and properties", schemas do
+    assert schemas.array.type == :list
+    assert schemas.array.properties == %List{items: nil}
+
+    assert schemas.length.type == :list
+  end
+
+  describe "validate/2" do
+    test "with an empty list", %{array: schema},
+      do: assert validate(schema, []) == :ok
+
+    test "with an list of different types", %{array: schema},
+      do: assert validate(schema, [1, "bla", 3.4]) == :ok
+
+    test "with different type", %{array: schema},
+      do: assert validate(schema, "not an array") == {:error, %{type: :list}}
+
+    test "to short list", %{length: schema},
+      do: assert validate(schema, [1]) == {:error, %{min_items: 2}}
+
+    test "proper list", %{length: schema},
+      do: assert validate(schema, [1, 2]) == :ok
+
+    test "to long list", %{length: schema},
+      do: assert validate(schema, [1, 2, 3, 4]) == {:error, %{max_items: 3}}
+
+    test "integer items with list of integers", %{integers: schema},
+      do: assert validate(schema, [1, 2]) == :ok
+
+    test "integer items with invalid list", %{integers: schema} do
+      expected = {
+        :error,
+        :nested,
+        %{
+          at: 2,
+          error: {:error, :wrong_type, %{type: :integer}}
+        }
+      }
+      assert validate(schema, [1, 2, "a", 3]) == expected
+    end
+
+    test "tuple with valid values", %{tuple: schema},
+      do: assert validate(schema, ["a", 2]) == :ok
+
+    test "tuple with invalid values", %{tuple: schema} do
+      expected = {
+        :error,
+        :nested,
+        %{
+          at: 0,
+          error: {:error, :wrong_type, %{type: :string}}
+        }
+      }
+      assert validate(schema, [2, "a"]) == expected
+    end
+
+    test "tuple with too less values", %{tuple: schema} do
+      expected = {:error, :missing_value, %{at: 1}}
+      assert validate(schema, ["a"]) == expected
+    end
+
+    test "tuple with more values", %{tuple: schema},
+      do: assert validate(schema, ["a", 2, "too many"]) == :ok
+
+    test "tuple with too many values", %{tuple_add: schema} do
+      expected = {:error, :extra_value, %{at: 2}}
+      assert validate(schema, ["a", 2, "too many"]) == expected
+    end
+
+    test "list with unique items", %{unique: schema},
+      do: assert validate(schema, [1, 2, 3]) == :ok
+
+    test "list with none unique items", %{unique: schema} do
+      expected = {:error, :not_unique, %{}}
+      assert validate(schema, [1, 2, 2]) == expected
+    end
+  end
+end
