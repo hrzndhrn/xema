dbf670ce162c8f853a03e1158cd3d1b7ec849bb3
Marcus Kruse <esurk.sucram@gmail.com>
2017-07-04 20:48:45 +0200

    Update Xeam.Map

--- lib/xema/map.ex
+++ lib/xema/map.ex
@@ -6,22 +6,53 @@ defmodule Xema.Map do
   @behaviour Xema
 
   defstruct as: :map,
-            string_keys: false
+            string_keys: false,
+            properties: nil
 
-  alias Xema.Map
+  @spec keywords(list) :: %Xema.Map{}
+  def keywords(keywords), do: struct(%Xema.Map{}, keywords)
 
-  @spec keywords(list) :: %Map{}
-  def keywords(keywords), do: struct(%Map{}, keywords)
-
-  @spec is_valid?(%Map{}, any) :: boolean
+  @spec is_valid?(%Xema.Map{}, any) :: boolean
   def is_valid?(keywords, map), do: validate(keywords, map) == :ok
 
-  @spec validate(%Map{}, any) :: :ok | {:error, atom, any}
+  @spec validate(%Xema.Map{}, any) :: :ok | {:error, atom, any}
   def validate(keywords, map) do
     with :ok <- type(keywords, map),
+         :ok <- properties(keywords, map),
       do: :ok
   end
 
   defp type(_keywords, map) when is_map(map), do: :ok
   defp type(keywords, _map), do: {:error, :wrong_type, %{type: keywords.as}}
+
+  defp properties(%Xema.Map{properties: nil}, _map), do: :ok
+  defp properties(%Xema.Map{properties: properties}, map) do
+    validate_properties(Map.to_list(properties), map)
+  end
+
+  defp validate_properties([], _map), do: :ok
+  defp validate_properties([{property, schema}|properties], map) do
+    case validate_property(schema, property, get_value(map, property)) do
+      :ok -> validate_properties(properties, map)
+      error -> error
+    end
+  end
+
+  defp validate_property(_schema, _propertey, nil), do: :ok
+  defp validate_property(schema, property, value) do
+    case Xema.validate(schema, value) do
+      :ok -> :ok
+      {:error, _, _} = error ->
+        {:error, :invalid_property, %{property: property, error: error}}
+    end
+  end
+
+  defp get_value(map, key) do
+    case {Map.get(map, key), Map.get(map, to_string key)} do
+      {nil, nil} -> nil
+      {nil, value} -> value
+      {value, nil} -> value
+      _ -> {:erro, :mixed_map}
+    end
+  end
 end
--- test/xema/map_test.exs
+++ test/xema/map_test.exs
@@ -4,13 +4,18 @@ defmodule Xema.MapTest do
 
   import Xema, only: [is_valid?: 2, validate: 2]
 
-  alias Xema.List
   alias Xema.Map
 
   setup do
     %{
       map: Xema.create(:map),
-      object: Xema.create(:map, as: :object)
+      object: Xema.create(:map, as: :object),
+      props: Xema.create(
+        :map,
+        properties: %{
+          foo: Xema.create(:number),
+          bar: Xema.create(:string)
+        })
     }
   end
 
@@ -35,5 +40,29 @@ defmodule Xema.MapTest do
       expected = {:error, :wrong_type, %{type: :object}}
       assert validate(schema, "foo") == expected
     end
+
+    test "properties with valid values", %{props: schema},
+      do: assert validate(schema, %{foo: 2, bar: "bar"}) == :ok
+
+    test "properties with invalid values", %{props: schema} do
+      expected = {
+        :error,
+        :invalid_property,
+        %{
+          property: :foo,
+          error: {:error, :wrong_type, %{type: :number}}
+        }
+      }
+      assert validate(schema, %{foo: "foo", bar: "bar"}) == expected
+    end
+
+  end
+
+  describe "is_valid/2" do
+    test "with an empty map", %{map: schema},
+      do: assert is_valid?(schema, %{})
+
+    test "map with a string", %{map: schema},
+      do: refute is_valid?(schema, "foo")
   end
 end
