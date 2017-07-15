a78867d22c6cdcde1acfd37d0127253cf04670e1
Marcus Kruse <esurk.sucram@gmail.com>
2017-07-15 22:17:02 +0200

    Add additional properties draft

--- lib/xema/map.ex
+++ lib/xema/map.ex
@@ -8,15 +8,22 @@ defmodule Xema.Map do
   @behaviour Xema
 
   defstruct [
-    :additonal_properties,
+    :additional_properties,
     :max_properties,
     :min_properties,
     :properties,
+    :required,
+    :pattern_properties,
     as: :map
   ]
 
   @spec keywords(keyword) :: %Xema.Map{}
-  def keywords(keywords), do: struct(%Xema.Map{}, keywords)
+  def keywords(keywords), do: struct(%Xema.Map{}, setup(keywords))
+
+  defp setup(keywords) do
+    keywords
+    |> Keyword.update(:required, nil, fn x -> MapSet.new(x) end)
+  end
 
   @spec is_valid?(%Xema{}, any) :: boolean
   def is_valid?(xema, map), do: validate(xema, map) == :ok
@@ -24,9 +31,10 @@ defmodule Xema.Map do
   @spec validate(%Xema{}, any) :: :ok | {:error, map}
   def validate(%Xema{keywords: keywords}, map) do
     with :ok <- type(keywords, map),
-         :ok <- properties(keywords, map),
          :ok <- size(keywords, map),
-         :ok <- additonal_properties(keywords, map),
+         :ok <- required(keywords, map),
+         {:ok, map} <- properties(keywords, map),
+         :ok <- additional_properties(keywords, map),
       do: :ok
   end
 
@@ -43,10 +51,10 @@ defmodule Xema.Map do
     validate_properties(Map.to_list(properties), map)
   end
 
-  defp validate_properties([], _map), do: :ok
+  defp validate_properties([], map), do: {:ok, map}
   defp validate_properties([{property, schema}|properties], map) do
     case validate_property(schema, property, get_value(map, property)) do
-      :ok -> validate_properties(properties, map)
+      :ok -> validate_properties(properties, Map.delete(map, property))
       error -> error
     end
   end
@@ -60,12 +68,35 @@ defmodule Xema.Map do
     end
   end
 
-  defp get_value(map, key) do
+  defp get_value(map, key) when is_atom(key) do
     case {Map.get(map, key), Map.get(map, to_string key)} do
       {nil, nil} -> nil
       {nil, value} -> value
       {value, nil} -> value
       _ -> {:erro, :mixed_map}
+    end
+  end
+  defp get_value(map, key) do
+    case {Map.get(map, key), Map.get(map, String.to_atom key)} do
+      {nil, nil} -> nil
+      {nil, value} -> value
+      {value, nil} -> value
+      _ -> {:erro, :mixed_map}
+    end
+  end
+  
+  defp required(%Xema.Map{required: nil}, _map), do: :ok
+  defp required(%Xema.Map{required: required}, map) do
+    properties = map |> Map.keys |> MapSet.new
+
+    if MapSet.subset?(required, properties) do 
+      :ok 
+    else 
+      error(
+        :missing_properties, 
+        missing: required |> MapSet.difference(properties) |> MapSet.to_list,
+        required: MapSet.to_list(required)
+      )
     end
   end
 
@@ -81,16 +112,26 @@ defmodule Xema.Map do
     do: error(:too_many_properties, max_properties: max)
   defp do_size(_len, _min, _max), do: :ok
 
-  defp additonal_properties(%Xema.Map{additonal_properties: nil}, _map), do: :ok
-  defp additonal_properties(
-    %Xema.Map{additonal_properties: false, properties: properties},
-    map
-  ) do
+  defp additional_properties(schema, map) do
+    case schema.additional_properties do
+      nil -> :ok
+
+      false -> 
+        if Map.equal?(map, %{}) do
+          :ok
+        else
+          error(:no_additional_properties_allowed, 
+                additional_properties: Map.keys(map))
+        end
+    end
+  end
+
+  defp do_additional_properties(props, nil, map) do
     additional_properties =
       map
       |> Map.keys
       |> MapSet.new
-      |> MapSet.difference(properties |> Map.keys |> MapSet.new)
+      |> MapSet.difference(props |> Map.keys |> MapSet.new)
       |> MapSet.to_list
 
     if Enum.empty?(additional_properties) do
@@ -98,7 +139,31 @@ defmodule Xema.Map do
     else
       error(
         :no_additional_properties_allowed,
-        additonal_properties: additional_properties)
+        additional_properties: additional_properties)
+    end
+  end
+  defp do_additional_properties(nil, patterns, map) do
+    properties =
+      map
+      |> Map.keys
+      |> Enum.map(fn x -> to_string x end)
+
+    patterns = Map.keys(patterns)
+      
+    do_additional_properties_match(properties, patterns)
+  end
+  defp do_additional_properties(props, patterns, map) do
+    with :ok <- do_additional_properties(props, nil, map),
+         :ok <- do_additional_properties(nil, patterns, map),
+      do: :ok
+  end
+
+  defp do_additional_properties_match([], _patterns), do: :ok
+  defp do_additional_properties_match([prop|props], patterns) do 
+    if Enum.any?(patterns, fn pattern -> Regex.match?(pattern, prop) end) do
+      do_additional_properties_match(props, patterns)
+    else
+      error(:no_pattern_match, property: prop, patterns: patterns)
     end
   end
 end
--- test/xema/map_test.exs
+++ test/xema/map_test.exs
@@ -12,11 +12,11 @@ defmodule Xema.MapTest do
       map: Xema.create(:map),
       # An empty map schema running as an object schema
       object: Xema.create(:map, as: :object),
-      # An map schema with min_properties
+      # A map schema with min_properties
       min: Xema.create(:map, min_properties: 2),
-      # An map schema with max_properties
+      # A map schema with max_properties
       max: Xema.create(:map, max_properties: 3),
-      # An map schema with min_properties and max_properties
+      # A map schema with min_properties and max_properties
       min_max: Xema.create(:map, min_properties: 2, max_properties: 3),
       # Just some properties
       props: Xema.create(
@@ -24,16 +24,54 @@ defmodule Xema.MapTest do
         properties: %{
           foo: Xema.create(:number),
           bar: Xema.create(:string)
-        }),
-      # Some properties and no additonal properties
+        }
+      ),
+      # Some properties and no additional properties
       no_add: Xema.create(
         :map,
-        additonal_properties: false,
+        additional_properties: false,
         properties: %{
           foo: Xema.create(:number),
           bar: Xema.create(:string)
-        })
-
+        }),
+      # Some propertieds ans some required properties
+      atom: %{
+        required: Xema.create(
+          :map,
+          required: [:foo, :bar],
+          properties: %{
+            foo: Xema.create(:number),
+            bar: Xema.create(:string),
+            baz: Xema.create(:number)
+          }
+        ),
+      },
+      string: %{
+        required: Xema.create(
+          :map,
+          required: ["foo", "bar"],
+          properties: %{
+            foo: Xema.create(:number),
+            bar:  Xema.create(:string),
+            baz: Xema.create(:number)
+          }
+        ),      
+        props: Xema.create(
+          :map,
+          properties: %{
+            "foo" => Xema.create(:number),
+            "bar" => Xema.create(:string)
+          }
+        )
+      },
+      pattern: Xema.create(
+        :map,
+        pattern_properties: %{
+          ~r/^s_/ => Xema.create(:string),
+          ~r/^i_/ => Xema.create(:number)
+        },
+        additional_properties: false
+      )
     }
   end
 
@@ -60,8 +98,15 @@ defmodule Xema.MapTest do
     assert validate(schema, "foo") == expected
   end
 
-  test "properties with valid values", %{props: schema},
-    do: assert validate(schema, %{foo: 2, bar: "bar"}) == :ok
+  test "properties with valid values", %{props: schema} do
+    assert validate(schema, %{foo: 2, bar: "bar"}) == :ok
+    assert validate(schema, %{"foo" => 2, "bar" => "bar"}) == :ok
+  end
+
+  test "properties with valid values (string)", %{string: %{props: schema}} do
+    assert validate(schema, %{foo: 2, bar: "bar"}) == :ok
+    assert validate(schema, %{"foo" => 2, "bar" => "bar"}) == :ok
+  end
 
   test "properties with invalid values", %{props: schema} do
     expected = {:error, %{
@@ -103,16 +148,54 @@ defmodule Xema.MapTest do
     assert validate(schema, %{a: 1, b: 2, c: 3, d: 4}) == expected
   end
 
-  describe "schema with no additonal properties" do
+  describe "schema with no additional properties" do
     test "with less properties", %{no_add: schema},
       do: assert validate(schema, %{foo: 1}) == :ok
 
-    test "with additonal property", %{no_add: schema} do
+    test "with additional property", %{no_add: schema} do
       expected = {:error, %{
         reason: :no_additional_properties_allowed,
-        additonal_properties: [:add]
+        additional_properties: [:add]
       }}
       assert validate(schema, %{add: 1}) == expected
+    end
+  end
+
+  describe "schema with required properties (atom keys)" do
+    test "with propper map", %{atom: %{required: schema}},
+      do: assert validate(schema, %{foo: 1, bar: "x"}) == :ok
+
+    test "with a missing property", %{atom: %{required: schema}} do
+      expected = {:error, %{
+        reason: :missing_properties, 
+        missing: [:bar], 
+        required: [:bar, :foo]
+      }}
+      assert validate(schema, %{foo: 1}) == expected
+    end
+  end
+
+  describe "schema with required properties (string keys)" do
+    test "with propper map", %{string: %{required: schema}},
+      do: assert validate(schema, %{"foo" => 1, "bar" => "x"}) == :ok
+
+    test "with a missing property", %{string: %{required: schema}} do
+      expected = {:error, %{
+        reason: :missing_properties, 
+        missing: ["bar"], 
+        required: ["bar", "foo"]
+      }}
+      assert validate(schema, %{"foo" => 1}) == expected
+    end
+  end
+
+  describe "schema with pattern properties" do
+    test "with propper map", %{pattern: schema},
+      do: assert validate(schema, %{i_0: 0, i_1: 1, s_X: "six"}) == :ok
+
+    test "with wrong value", %{pattern: schema} do
+      expected = {:error, %{reason: :invalid_property, property: :i_0}}
+      assert validate(schema, %{i_0: "bla"}) == expected
     end
   end
 
