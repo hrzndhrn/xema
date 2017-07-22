4ef38930a64e51f529529aebe78b308cedfe569c
Marcus Kruse <esurk.sucram@gmail.com>
2017-07-22 10:04:27 +0200

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -2,12 +2,21 @@ defmodule Xema.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :xema,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :xema,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
   end
 
   def application do
@@ -19,6 +28,7 @@ defmodule Xema.Mixfile do
       {:credo, "~> 0.8", only: [:dev, :test]},
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.7", only: :test},
       {:exfmt, "~> 0.2.3", only: [:dev, :test]}
     ]
   end
