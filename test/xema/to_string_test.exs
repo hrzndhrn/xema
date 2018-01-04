f3111cd673e0379180d4bed8c8d1a352c02553a8
Marcus Kruse <esurk.sucram@gmail.com>
2018-01-04 21:19:26 +0100

    Update tests

--- test/xema/to_string_test.exs
+++ test/xema/to_string_test.exs
@@ -42,12 +42,27 @@ defmodule Xema.ToStringTest do
   end
 
   describe "Xema.to_string" do
-    test "with a simple any-schema and an id" do
-      schema = ~s(:any, id: "foo")
+    test "with a simple any-schema" do
+      schema = ~s(:any)
       xema = xema(schema)
 
       assert Xema.to_string(xema) == to_string(xema)
       assert to_string(xema) == "xema(#{schema})"
+    end
+
+    test "with a simple any-schema with an id and as :bar" do
+      schema = ~s(:any, as: :bar, id: "foo")
+      xema = xema(schema)
+
+      assert to_string(xema) == "xema(#{schema})"
+    end
+
+    test "with an enum" do
+      schema = ~s(:enum, [1, 2, 3])
+      xema = xema(schema)
+
+      # Shortcuts will expand to the equivalent schema.
+      assert to_string(xema) == "xema(:any, enum: [1, 2, 3])"
     end
 
     test "with a integer-schema and keywords" do
