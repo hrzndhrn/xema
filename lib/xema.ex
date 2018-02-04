ae9e5bf40ae7422d4eb140878534f6df9e9d09b1
Marcus Kruse <esurk.sucram@gmail.com>
2018-02-04 13:36:54 +0100

    Refactoring Xema.Schema

--- lib/xema.ex
+++ lib/xema.ex
@@ -177,7 +177,7 @@ defmodule Xema do
       opts = Keyword.put(opts, :type, unquote(type))
 
       case SchemaValidator.validate(unquote(type), opts) do
-        :ok -> Schema.new(opts)
+        :ok -> opts |> update() |> Schema.new()
         {:error, msg} -> raise SchemaError, message: msg
       end
     end
@@ -198,6 +198,53 @@ defmodule Xema do
       message: "#{inspect(type)} is not a valid type or keyword."
   end
 
+  @spec update(keyword) :: keyword
+  defp update(opts) do
+    opts
+    |> Keyword.put_new(:as, opts[:type])
+    |> Keyword.update(:additional_items, nil, &bool_or_schema/1)
+    |> Keyword.update(:additional_properties, nil, &bool_or_schema/1)
+    |> Keyword.update(:all_of, nil, &schemas/1)
+    |> Keyword.update(:any_of, nil, &schemas/1)
+    |> Keyword.update(:dependencies, nil, &dependencies/1)
+    |> Keyword.update(:items, nil, &items/1)
+    |> Keyword.update(:not, nil, fn schema -> Xema.schema(schema) end)
+    |> Keyword.update(:one_of, nil, &schemas/1)
+    |> Keyword.update(:pattern_properties, nil, &properties/1)
+    |> Keyword.update(:properties, nil, &properties/1)
+    |> Keyword.update(:required, nil, &MapSet.new(&1))
+  end
+
+  @spec schemas(list) :: list
+  defp schemas(list), do: Enum.map(list, fn schema -> Xema.schema(schema) end)
+
+  @spec properties(map) :: map
+  defp properties(map),
+    do: Enum.into(map, %{}, fn {key, prop} -> {key, Xema.schema(prop)} end)
+
+  @spec dependencies(map) :: map
+  defp dependencies(map),
+    do:
+      Enum.into(map, %{}, fn
+        {key, dep} when is_list(dep) -> {key, dep}
+        {key, dep} -> {key, Xema.schema(dep)}
+      end)
+
+  @spec bool_or_schema(boolean | atom) :: boolean | Xema.Schema.t()
+  defp bool_or_schema(bool) when is_boolean(bool), do: bool
+
+  defp bool_or_schema(schema), do: Xema.schema(schema)
+
+  defp items(schema) when is_atom(schema) or is_tuple(schema),
+    do: Xema.schema(schema)
+
+  defp items(schemas) when is_list(schemas), do: schemas(schemas)
+
+  defp items(items), do: items
+
+  #
+  # to_string
+  #
   @spec to_string(Xema.t(), keyword) :: String.t()
   def to_string(%Xema{} = xema, opts \\ []) do
     format = Keyword.get(opts, :format, :call)
--- lib/xema/schema.ex
+++ lib/xema/schema.ex
@@ -121,51 +121,7 @@ defmodule Xema.Schema do
   ]
 
   @spec new(keyword) :: Xema.Schema.t()
-  def new(opts \\ []), do: struct(Xema.Schema, update(opts))
-
-  @spec update(keyword) :: keyword
-  defp update(opts) do
-    opts
-    |> Keyword.put_new(:as, opts[:type])
-    |> Keyword.update(:additional_items, nil, &bool_or_schema/1)
-    |> Keyword.update(:additional_properties, nil, &bool_or_schema/1)
-    |> Keyword.update(:all_of, nil, &schemas/1)
-    |> Keyword.update(:any_of, nil, &schemas/1)
-    |> Keyword.update(:dependencies, nil, &dependencies/1)
-    |> Keyword.update(:items, nil, &items/1)
-    |> Keyword.update(:not, nil, fn schema -> Xema.schema(schema) end)
-    |> Keyword.update(:one_of, nil, &schemas/1)
-    |> Keyword.update(:pattern_properties, nil, &properties/1)
-    |> Keyword.update(:properties, nil, &properties/1)
-    |> Keyword.update(:required, nil, &MapSet.new(&1))
-  end
-
-  @spec schemas(list) :: list
-  defp schemas(list), do: Enum.map(list, fn schema -> Xema.schema(schema) end)
-
-  @spec properties(map) :: map
-  defp properties(map),
-    do: Enum.into(map, %{}, fn {key, prop} -> {key, Xema.schema(prop)} end)
-
-  @spec dependencies(map) :: map
-  defp dependencies(map),
-    do:
-      Enum.into(map, %{}, fn
-        {key, dep} when is_list(dep) -> {key, dep}
-        {key, dep} -> {key, Xema.schema(dep)}
-      end)
-
-  @spec bool_or_schema(boolean | atom) :: boolean | Xema.Schema.t()
-  defp bool_or_schema(bool) when is_boolean(bool), do: bool
-
-  defp bool_or_schema(schema), do: Xema.schema(schema)
-
-  defp items(schema) when is_atom(schema) or is_tuple(schema),
-    do: Xema.schema(schema)
-
-  defp items(schemas) when is_list(schemas), do: schemas(schemas)
-
-  defp items(items), do: items
+  def new(opts \\ []), do: struct(Xema.Schema, opts)
 
   @spec to_string(any, keyword) :: String.t()
   def to_string(schema, opts \\ [])
