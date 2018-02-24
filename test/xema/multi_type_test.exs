79837478bff611d5f719d3d56595ab97c4bf54d5
Marcus Kruse <esurk.sucram@gmail.com>
2018-02-24 16:27:45 +0100

    Add multi type test file

---
+++ test/xema/multi_type_test.exs
@@ -0,0 +1,54 @@
+defmodule Xema.MultiTypeTest do
+  use ExUnit.Case, async: true
+
+  import Xema, only: [validate: 2]
+
+  test "&new/1 called with a wrong type list raised an exception" do
+    assert_raise ArgumentError, fn ->
+      Xema.new([:string, :foo])
+    end
+  end
+
+  describe "schema with type string or nil:" do
+    setup do
+      %{schema: Xema.new([:string, nil], min_length: 5)}
+    end
+
+    test "with a string", %{schema: schema} do
+      assert validate(schema, "foobar") == :ok
+    end
+
+    test "with an invalid string", %{schema: schema} do
+      assert validate(schema, "foo") == {:error, %{min_length: 5, value: "foo"}}
+    end
+
+    test "with nil", %{schema: schema} do
+      assert validate(schema, nil) == :ok
+    end
+
+    test "with integer", %{schema: schema} do
+      assert validate(schema, 42) ==
+               {:error, %{type: [:string, nil], value: 42}}
+    end
+  end
+
+  describe "property with type number or nil:" do
+    setup do
+      %{schema: Xema.new(:properties, %{foo: [:number, nil]})}
+    end
+
+    test "with a number", %{schema: schema} do
+      assert validate(schema, %{foo: 42}) == :ok
+    end
+
+    test "with nil", %{schema: schema} do
+      assert validate(schema, %{foo: nil}) == :ok
+    end
+
+    test "with a string", %{schema: schema} do
+      assert validate(schema, %{foo: "foo"}) ==
+               {:error,
+                %{properties: %{foo: %{type: [:number, nil], value: "foo"}}}}
+    end
+  end
+end
