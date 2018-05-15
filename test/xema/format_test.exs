068292557605ddcc15c028c78ee9794c6936142e
Marcus Kruse <esurk.sucram@gmail.com>
2018-05-15 19:53:45 +0200

    Add format test

---
+++ test/xema/format_test.exs
@@ -0,0 +1,19 @@
+defmodule FormatTest do
+  use ExUnit.Case, async: true
+
+  import Xema, only: [validate: 2]
+
+  describe "validation of date-time strings" do
+    setup do
+      %{schema: Xema.new(:format, :date_time)}
+    end
+
+    test "a invalid day in date-time string", %{schema: schema} do
+      data = "1990-02-31T15:59:60.123-08:00"
+      assert validate(schema, data) == {:error,
+             %{format: :date_time, value: "1990-02-31T15:59:60.123-08:00"}}
+
+    end
+  end
+end
+
