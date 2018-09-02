a3ed56e4f23c019c627f3cbecddf3b739e82214d
Marcus Kruse <esurk.sucram@gmail.com>
2018-09-02 14:38:53 +0200

    Add uri-reference tests

--- test/suite/draft6/optional/format_test.exs
+++ test/suite/draft6/optional/format_test.exs
@@ -160,6 +160,47 @@ defmodule Draft6.Optional.FormatTest do
     end
   end
 
+  describe "validation of URI References" do
+    setup do
+      %{schema: Xema.new(:format, :uri_reference)}
+    end
+
+    test "a valid URI", %{schema: schema} do
+      data = "http://foo.bar/?baz=qux#quux"
+      assert is_valid?(schema, data)
+    end
+
+    test "a valid protocol-relative URI Reference", %{schema: schema} do
+      data = "//foo.bar/?baz=qux#quux"
+      assert is_valid?(schema, data)
+    end
+
+    test "a valid relative URI Reference", %{schema: schema} do
+      data = "/abc"
+      assert is_valid?(schema, data)
+    end
+
+    test "an invalid URI Reference", %{schema: schema} do
+      data = "\\\\WINDOWS\\fileshare"
+      refute is_valid?(schema, data)
+    end
+
+    test "a valid URI Reference", %{schema: schema} do
+      data = "abc"
+      assert is_valid?(schema, data)
+    end
+
+    test "a valid URI fragment", %{schema: schema} do
+      data = "#fragment"
+      assert is_valid?(schema, data)
+    end
+
+    test "an invalid URI fragment", %{schema: schema} do
+      data = "#frag\\ment"
+      refute is_valid?(schema, data)
+    end
+  end
+
   describe "validation of e-mail addresses" do
     setup do
       %{schema: Xema.new(:format, :email)}
