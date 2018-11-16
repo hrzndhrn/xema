5612d276942316381e12999c26af595ee8c5df1b
Marcus Kruse <esurk.sucram@gmail.com>
2018-11-16 20:08:57 +0100

    Update dialyzer

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Xema.Mixfile do
     [
       {:cowboy, "~> 2.4", only: :test},
       {:credo, "~> 0.10", only: [:dev, :test]},
-      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
+      # {:dialyxir, "~> 1.0.0~rc.4", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:excoveralls, "~> 0.9", only: :test},
       {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test, :docs]},
