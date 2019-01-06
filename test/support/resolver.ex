bf889e8b446f53e46d1761d612b3ea84add0cdf1
Marcus Kruse <esurk.sucram@gmail.com>
2019-01-06 10:19:41 +0100

    Update test resolver

--- test/support/resolver.ex
+++ test/support/resolver.ex
@@ -3,31 +3,66 @@ defmodule Test.Resolver do
 
   @behaviour Xema.Resolver
 
-  @spec fetch(binary) :: {:ok, map} | {:error, any}
-  def fetch(uri) do
-    with {:ok, response} <- get(uri), do: eval(response, uri)
-  end
+  defmodule RemoteResolver do
+    @moduledoc false
 
-  defp get(uri) do
-    case HTTPoison.get(uri) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, body}
+    @behaviour Xema.Resolver
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, "Remote schema '#{uri}' not found."}
+    @spec fetch(binary) :: {:ok, map} | {:error, any}
+    def fetch(uri) do
+      with {:ok, response} <- get(uri), do: eval(response, uri)
+    end
 
-      {:ok, %HTTPoison.Response{status_code: code}} ->
-        {:error, "code: #{code}"}
+    defp get(uri) do
+      case HTTPoison.get(uri) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          {:ok, body}
 
-      {:error, reason} ->
-        {:error, reason}
+        {:ok, %HTTPoison.Response{status_code: 404}} ->
+          {:error, "Remote schema '#{uri}' not found."}
+
+        {:ok, %HTTPoison.Response{status_code: code}} ->
+          {:error, "code: #{code}"}
+
+        {:error, reason} ->
+          {:error, reason}
+      end
+    end
+
+    defp eval(str, uri) do
+      {data, _} = Code.eval_string(str)
+      {:ok, data}
+    rescue
+      error -> {:error, %{error | file: URI.to_string(uri)}}
     end
   end
 
-  defp eval(str, uri) do
-    {data, _} = Code.eval_string(str)
-    {:ok, data}
-  rescue
-    error -> {:error, %{error | file: URI.to_string(uri)}}
+  defmodule FileResolver do
+    @moduledoc false
+
+    @behaviour Xema.Resolver
+
+    @spec fetch(binary) :: {:ok, map} | {:error, any}
+    def fetch(uri),
+      do:
+        "test/support/remote"
+        |> Path.join(uri.path)
+        |> File.read!()
+        |> eval(uri)
+
+    defp eval(str, uri) do
+      {data, _} = Code.eval_string(str)
+      {:ok, data}
+    rescue
+      error -> {:error, %{error | file: URI.to_string(uri)}}
+    end
+  end
+
+  @spec fetch(binary) :: {:ok, map} | {:error, any}
+  def fetch(uri) do
+    case uri.host do
+      nil -> FileResolver.fetch(uri)
+      _ -> RemoteResolver.fetch(uri)
+    end
   end
 end
