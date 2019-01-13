849fb6ba7a305b4e1363505b726f9582e1f6bb90
Marcus Kruse <esurk.sucram@gmail.com>
2019-01-13 16:33:42 +0100

    Add Format.formats

--- lib/xema/format.ex
+++ lib/xema/format.ex
@@ -55,6 +55,11 @@ defmodule Xema.Format do
   defguard supports(format) when format in @formats
 
   @doc """
+  Returns all available formats.
+  """
+  def formats, do: @formats
+
+  @doc """
   Checks if the value matches the given type. The function expected a available
   `format` and a `string` to check. Returns `true` for a valid `string`, `false`
   otherwise.
