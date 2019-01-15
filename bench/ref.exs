f5824e4a58005c86ea6160543ca662b30119befd
Marcus Kruse <esurk.sucram@gmail.com>
2019-01-15 20:36:21 +0100

    Add benchmark

---
+++ bench/ref.exs
@@ -0,0 +1,39 @@
+defmodule Bench do
+  defp fun do
+    schema =
+      Xema.new(
+        {:map,
+         definitions: %{
+           neg: {:integer, maximum: 0},
+           pos: {:integer, minimum: 0}
+         },
+         properties: %{
+           neg: {:ref, "#/definitions/neg"},
+           pos: {:ref, "#/definitions/pos"}
+         }}
+      )
+
+    data = %{neg: -5, pos: 6}
+
+    fn -> true = Xema.valid?(schema, data) end
+  end
+
+  def run do
+    Benchee.run(
+      %{"ref" => fun},
+      parallel: 4,
+      print: [fast_warning: false],
+      formatters: [
+        # &Benchee.Formatters.HTML.output/1,
+        &Benchee.Formatters.Console.output/1
+      ],
+      formatter_options: [
+        html: [
+          file: Path.expand("output/bench.html", __DIR__)
+        ]
+      ]
+    )
+  end
+end
+
+Bench.run()
