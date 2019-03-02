98ca24e64371e381bbf8f154adf9b7abd396c0a3
Marcus Kruse <esurk.sucram@gmail.com>
2019-03-02 08:56:06 +0100

    Update benchmark

--- bench/ref.exs
+++ bench/ref.exs
@@ -22,17 +22,11 @@ defmodule Bench do
     Benchee.run(
       %{"ref" => fun()},
       parallel: 4,
-      save: [path: "bench/tmp/ref.benchee", tag: "master"],
+      # save: [path: "bench/tmp/ref.benchee", tag: "master"],
       print: [fast_warning: false],
       load: "bench/tmp/ref.benchee",
       formatters: [
-        # &Benchee.Formatters.HTML.output/1,
-        &Benchee.Formatters.Console.output/1
-      ],
-      formatter_options: [
-        html: [
-          file: Path.expand("output/bench.html", __DIR__)
-        ]
+        Benchee.Formatters.Console
       ]
     )
   end
