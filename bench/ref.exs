1995d4d66472cc0b48efe2c1c9c9e20ae6f14384
Marcus Kruse <esurk.sucram@gmail.com>
2019-03-03 11:31:04 +0100

    Update benchmark

--- bench/ref.exs
+++ bench/ref.exs
@@ -1,4 +1,45 @@
 defmodule Bench do
+  defmodule GitStrategy do
+    @file_name "benchee.run"
+
+    defp app, do: to_string(Mix.Project.config()[:app])
+
+    defp branch,
+      do:
+        "git"
+        |> System.cmd(["rev-parse", "--abbrev-ref", "HEAD"])
+        |> trim()
+
+    defp trim({str, 0}), do: String.trim(str)
+
+    def load(branch \\ "master") do
+      case branch == branch() do
+        true ->
+          nil
+
+        false ->
+          System.tmp_dir!()
+          |> Path.join(app())
+          |> Path.join(branch)
+          |> Path.join(@file_name)
+      end
+    end
+
+    def save do
+      tag = branch()
+
+      path =
+        System.tmp_dir!()
+        |> Path.join(app())
+        |> Path.join(tag)
+        |> Path.join(@file_name)
+
+      [path: path, tag: tag]
+    end
+
+    def config, do: [load: load(), save: save()]
+  end
+
   def fun do
     schema =
       Xema.new(
@@ -19,15 +60,18 @@ defmodule Bench do
   end
 
   def run do
+    config =
+      [
+        parallel: 4,
+        print: [fast_warning: false],
+        formatters: [
+          Benchee.Formatters.Console
+        ]
+      ] ++ GitStrategy.config()
+
     Benchee.run(
       %{"ref" => fun()},
-      parallel: 4,
-      # save: [path: "bench/tmp/ref.benchee", tag: "master"],
-      print: [fast_warning: false],
-      load: "bench/tmp/ref.benchee",
-      formatters: [
-        Benchee.Formatters.Console
-      ]
+      config
     )
   end
 end
