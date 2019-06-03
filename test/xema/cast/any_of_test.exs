61f5ef6ed931f8fabf98799efb2cef269d918234
Marcus Kruse <esurk.sucram@gmail.com>
2019-06-03 21:09:39 +0200

    Add tests

---
+++ test/xema/cast/any_of_test.exs
@@ -0,0 +1,108 @@
+defmodule Xema.Cast.AnyOfTest do
+  use ExUnit.Case, async: true
+
+  import Xema, only: [cast: 2]
+
+  alias Xema.CastError
+
+  describe "cast/2 with any_of schema with types" do
+    setup do
+      %{
+        schema: Xema.new(any_of: [:integer, :string, nil])
+      }
+    end
+
+    test "from an integer", %{schema: schema} do
+      assert cast(schema, 6) == {:ok, 6}
+    end
+
+    test "from an integer string", %{schema: schema} do
+      assert cast(schema, "9") == {:ok, 9}
+    end
+
+    test "from a string", %{schema: schema} do
+      assert cast(schema, "nine") == {:ok, "nine"}
+    end
+
+    test "from a nil", %{schema: schema} do
+      assert cast(schema, nil) == {:ok, nil}
+    end
+
+    test "from a float", %{schema: schema} do
+      assert cast(schema, 5.5) == {:ok, "5.5"}
+    end
+
+    test "from an empty list", %{schema: schema} do
+      assert {:error, error} = cast(schema, [])
+      assert error == %CastError{path: [], to: [:integer, :string, nil], value: []}
+      assert Exception.message(error) == "cannot cast [] to any of [:integer, :string, nil]"
+    end
+  end
+
+  describe "cast/2 with any_of schema with properties" do
+    setup do
+      %{
+        schema:
+          Xema.new(
+            any_of: [
+              [properties: %{a: :string}],
+              [properties: %{b: :integer}]
+            ]
+          )
+      }
+    end
+
+    test "from a map", %{schema: schema} do
+      assert cast(schema, %{a: 1, b: "2"}) == {:ok, %{a: "1", b: 2}}
+    end
+
+    test "from a map with an invalid value", %{schema: schema} do
+      assert {:error, error} = cast(schema, %{a: 1, b: 1.5})
+      assert error == %Xema.CastError{path: [:b], to: :integer, value: 1.5}
+      assert Exception.message(error) == "cannot cast 1.5 to :integer at [:b]"
+    end
+
+    test "from a keyword list", %{schema: schema} do
+      assert cast(schema, a: 1, b: "2") == {:ok, [a: "1", b: 2]}
+    end
+  end
+
+  describe "cast/2 with any_of schema with multiple properties" do
+    setup do
+      %{
+        schema:
+          Xema.new(
+            any_of: [
+              [properties: %{a: :integer}],
+              [properties: %{a: :string}],
+              [properties: %{a: nil}]
+            ]
+          )
+      }
+    end
+
+    test "from a map with an integer", %{schema: schema} do
+      assert cast(schema, %{a: 1}) == {:ok, %{a: 1}}
+    end
+
+    test "from a map with an integer string", %{schema: schema} do
+      assert cast(schema, %{a: "2"}) == {:ok, %{a: 2}}
+    end
+
+    test "from a map with a string", %{schema: schema} do
+      assert cast(schema, %{a: "three"}) == {:ok, %{a: "three"}}
+    end
+
+    test "from a map with a nil", %{schema: schema} do
+      assert cast(schema, %{a: nil}) == {:ok, %{a: nil}}
+    end
+
+    test "from a map with an empty list", %{schema: schema} do
+      assert {:error, error} = cast(schema, %{a: []})
+      assert error == %CastError{path: [:a], to: [:integer, :string, nil], value: []}
+
+      assert Exception.message(error) ==
+               "cannot cast [] to any of [:integer, :string, nil] at [:a]"
+    end
+  end
+end
