120819e94d8d96692ff9f934a5f7e7a5126115e9
Marcus Kruse <esurk.sucram@gmail.com>
2019-06-10 12:13:09 +0200

    Update .dialyzer_ignore.exs

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -2,7 +2,7 @@
   # Ignore warnings caused by the following problem:
   # https://github.com/elixir-lang/elixir/issues/7177
   {"lib/xema/schema.ex", :pattern_match, 323},
-  {"lib/xema/validator.ex", :pattern_match, 409},
+  {"lib/xema/validator.ex", :pattern_match, 368},
 
   # due a bug in dialyzer
   ~r/.*Function.Xema.Castable.*.__impl__.1.does.not.exist.*/
