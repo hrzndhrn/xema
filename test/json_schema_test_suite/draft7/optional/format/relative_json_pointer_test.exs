ca4c82178be187b54d839127ce0447b4546af48a
Marcus Kruse <esurk.sucram@gmail.com>
2019-09-15 12:40:43 +0200

    Add json pointer test

---
+++ test/json_schema_test_suite/draft7/optional/format/relative_json_pointer_test.exs
@@ -0,0 +1,31 @@
+defmodule JsonSchemaTestSuite.Draft7.Optional.Format.RelativeJsonPointer do
+  use ExUnit.Case
+
+  import Xema, only: [valid?: 2]
+
+  describe "validation of Relative JSON Pointers (RJP)" do
+    setup do
+      %{schema: Xema.from_json_schema(%{"format" => "relative-json-pointer"})}
+    end
+
+    test "a valid upwards RJP", %{schema: schema} do
+      assert valid?(schema, "1")
+    end
+
+    test "a valid downwards RJP", %{schema: schema} do
+      assert valid?(schema, "0/foo/bar")
+    end
+
+    test "a valid up and then down RJP, with array index", %{schema: schema} do
+      assert valid?(schema, "2/0/baz/1/zip")
+    end
+
+    test "a valid RJP taking the member or index name", %{schema: schema} do
+      assert valid?(schema, "0#")
+    end
+
+    test "an invalid RJP that is a valid JSON Pointer", %{schema: schema} do
+      refute valid?(schema, "/foo/bar")
+    end
+  end
+end
