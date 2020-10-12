3a69163143c7b4d4e70b6f8c3f6e51f2d0aa9699
Marcus Kruse <esurk.sucram@gmail.com>
2020-10-12 20:14:18 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Xema.Mixfile do
   def project do
     [
       app: :xema,
-      version: "0.13.2",
+      version: "0.13.3",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
