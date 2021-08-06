2b6d05a3d45dc8d9cfedca363a5ced9114c1f54f
Marcus Kruse <esurk.sucram@gmail.com>
2021-08-06 08:09:25 +0200

    Update .dialyzer_ignore.exs

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,7 +1,7 @@
 [
   # Ignore warnings caused by the following problem:
   # https://github.com/elixir-lang/elixir/issues/6738
-  {"lib/xema/schema.ex", :pattern_match, 329},
+  {"lib/xema/schema.ex", :pattern_match, 324},
   {"lib/xema/validator.ex", :pattern_match, 426},
   {"lib/xema/schema.ex", :pattern_match, 0},
   {"lib/xema/validator.ex", :pattern_match, 0}
