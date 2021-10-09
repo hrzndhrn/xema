8ce717f3492c8fdfa9bae86f053f3a3d1f1cfa0e
Marcus Kruse <esurk.sucram@gmail.com>
2021-10-09 09:02:26 +0200

    Run mix format

--- test/xema/use/cast_test.exs
+++ test/xema/use/cast_test.exs
@@ -21,14 +21,14 @@ defmodule Xema.Use.CastTest do
       {:ok, datetime, 0} = DateTime.from_iso8601("1984-03-04 13:37:00.000000Z")
 
       assert Bar.cast!(%{time: "1984-03-04 13:37:00.000000Z"}) ==
-        %Bar{time: datetime}
+               %Bar{time: datetime}
     end
 
     test "cast!/1 from a map with string keys" do
       {:ok, datetime, 0} = DateTime.from_iso8601("1984-03-04 13:37:00.000000Z")
 
       assert Bar.cast!(%{"time" => "1984-03-04 13:37:00.000000Z"}) ==
-        %Bar{time: datetime}
+               %Bar{time: datetime}
     end
   end
 end
