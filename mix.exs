4346607642c01ec288741077792743599dae096a
Marcus Kruse <esurk.sucram@gmail.com>
2024-03-17 09:18:21 +0100

    Support last 5 minor versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Xema.Mixfile do
     [
       app: :xema,
       version: "0.17.1",
-      elixir: "~> 1.8",
+      elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
